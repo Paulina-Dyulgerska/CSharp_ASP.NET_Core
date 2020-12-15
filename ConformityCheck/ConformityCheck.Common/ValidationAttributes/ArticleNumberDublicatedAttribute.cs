26c0d1ce486f26c45fe061f958f3a067478bcd7c
Paulina-Dyulgerska <paylina_st@yahoo.com>
2020-12-15 22:41:17 +0200

    Supplier Conformity Edit

---
+++ ConformityCheck/ConformityCheck.Common/ValidationAttributes/ArticleNumberDublicatedAttribute.cs
@@ -0,0 +1,29 @@
+﻿namespace ConformityCheck.Common.ValidationAttributes
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using ConformityCheck.Services;
+
+    public class ArticleNumberDublicatedAttribute : ValidationAttribute
+    {
+        public ArticleNumberDublicatedAttribute()
+        {
+            this.ErrorMessage = $"There is already an article with this number.";
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var context = (IContentCheckService)validationContext.GetService(typeof(IContentCheckService));
+
+            var hasNumber = context
+                .ArticleEntityNumberCheck(value.ToString());
+
+            if (hasNumber)
+            {
+                return new ValidationResult(this.ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ ConformityCheck/ConformityCheck.Common/ValidationAttributes/ConformityTypeEntityDescriptionDublicatedAttribute.cs
@@ -0,0 +1,29 @@
+﻿namespace ConformityCheck.Common.ValidationAttributes
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using ConformityCheck.Services;
+
+    public class ConformityTypeEntityDescriptionDublicatedAttribute : ValidationAttribute
+    {
+        public ConformityTypeEntityDescriptionDublicatedAttribute()
+        {
+            this.ErrorMessage = $"There is already a conformity type with this description.";
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var context = (IContentCheckService)validationContext.GetService(typeof(IContentCheckService));
+
+            var hasDescription = context
+                .ConformityTypeEntityDescriptionCheck(value.ToString());
+
+            if (hasDescription)
+            {
+                return new ValidationResult(this.ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
