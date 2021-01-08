65c4bd4b2cacf928135f85c67d61a729cf105c27
Paulina-Dyulgerska <paylina_st@yahoo.com>
2021-01-08 18:54:53 +0200

    Article search

---
+++ ConformityCheck/Web/ConformityCheck.Web.ViewModels/Suppliers/SuppliersListAllModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ConformityCheck.Web.ViewModels.Suppliers
+{
+    using System.Collections.Generic;
+
+    using ConformityCheck.Data.Models;
+    using ConformityCheck.Services.Mapping;
+
+    public class SuppliersListAllModel : PagingViewModel
+    {
+        public IEnumerable<SupplierFullInfoModel> Suppliers { get; set; }
+    }
+}
