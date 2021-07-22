35ed37f2a27e1c2564498b85384180f06db38a3a
Paulina-Dyulgerska <paylina_st@yahoo.com>
2021-07-22 14:01:17 +0300

    Email RegEx custom for input models add

--- ConformityCheck/Web/ConformityCheck.Web.ViewModels/Suppliers/SupplierEditInputModel.cs
+++ ConformityCheck/Web/ConformityCheck.Web.ViewModels/Suppliers/SupplierEditInputModel.cs
@@ -23,20 +23,27 @@
         [Required]
         [MaxLength(50)]
         [EmailAddress]
+        [RegularExpression(
+            "^(?:[a-zA-Z0-9][a-zA-Z0-9_.-]+@(?:[a-zA-Z0-9-_]{2,}[.][a-zA-Z0-9-_]{2,}))(?:.[a-zA-Z0-9-_]{2,})?$",
+            ErrorMessage = "The Email field is not a valid e-mail address.")]
         public string Email { get; set; }
 
         [MaxLength(20)]
         [RegularExpression("[0-9+ -]*", ErrorMessage = "The field Phone Number could contain only digits, '-', '+' or ' '.")]
+        [Display(Name= "Phone number")]
         public string PhoneNumber { get; set; }
 
         [MaxLength(20)]
         [RegularExpression("^[A-Z]+[a-z]*$", ErrorMessage = "The field Contact Person first name could contain only letters.")]
+        [Display(Name= "Contact person first name")]
         public string ContactPersonFirstName { get; set; }
 
         [MaxLength(20)]
         [RegularExpression("^[A-Z]+[a-z]*$", ErrorMessage = "The field Contact Person last name could contain only letters.")]
+        [Display(Name= "Contact person last name")]
         public string ContactPersonLastName { get; set; }
 
+        [Display(Name= "Creator username")]
         public string UserEmail { get; set; }
     }
 }
